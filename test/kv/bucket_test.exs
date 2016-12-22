aa9684e889c94e722b4cf4a1a3c557872b6706d2
Ryan Tanay <ryantanay@newevolution.org>
2016-12-22 14:36:41 -0600

    Added deletion test

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -7,10 +7,16 @@ defmodule KV.BucketTest do
   end
 
   test "stores values by key", %{bucket: bucket} do
-    {:ok, bucket} = KV.Bucket.start_link
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "removes keys returning their value", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 12)
+
+    assert KV.Bucket.delete(bucket, "eggs") == 12
+    assert KV.Bucket.get(bucket, "eggs") == nil
   end
 end
