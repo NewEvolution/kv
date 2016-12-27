bb293500969704f9085a937eaf8d063969ee63f6
Ryan Tanay <ryantanay@newevolution.org>
2016-12-27 09:54:38 -0600

    Added bucket creation via supervisor to the registry module

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -49,7 +49,7 @@ defmodule KV.Registry do
     if Map.has_key?(names, name) do
       {:noreply, {names, refs}}
     else
-      {:ok, bucket} = KV.Bucket.start_link
+      {:ok, bucket} = KV.Bucket.Supervisor.start_bucket
       ref = Process.monitor(bucket)
       refs = Map.put(refs, ref, name)
       names = Map.put(names, name, bucket)
