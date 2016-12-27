dc634b196ee3178ec25e651f8078154c074a67fd
Ryan Tanay <ryantanay@newevolution.org>
2016-12-27 10:33:17 -0600

    Storing buckets in ETS

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -7,7 +7,7 @@ defmodule KV.Registry do
   Starts the registry.
   """
   def start_link(name) do
-    GenServer.start_link(__MODULE__, :ok, name: name)
+    GenServer.start_link(__MODULE__, name: name)
   end
 
   @doc """
@@ -15,8 +15,11 @@ defmodule KV.Registry do
 
   Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
   """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+  def lookup(server, name) when is_atom(server) do
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
   end
 
   @doc """
@@ -35,31 +38,28 @@ defmodule KV.Registry do
 
   ## Server Callbacks
 
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
     {:ok, {names, refs}}
   end
 
-  def handle_call({:lookup, name}, _from, {names, _} = state) do
-    {:reply, Map.fetch(names, name), state}
-  end
-
   def handle_cast({:create, name}, {names, refs}) do
-    if Map.has_key?(names, name) do
-      {:noreply, {names, refs}}
-    else
-      {:ok, bucket} = KV.Bucket.Supervisor.start_bucket
-      ref = Process.monitor(bucket)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, bucket)
-      {:noreply, {names, refs}}
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:noreply, {names, refs}}
+      :error ->
+        {:ok, pid} = KV.Bucket.Supervisor.start_bucket
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:noreply, {names, refs}}
     end
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
+    :ets.delete(names, name)
     {:noreply, {names, refs}}
   end
 
